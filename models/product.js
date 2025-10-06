8fcdcd2f1b5f685e6085a27bfa337a65788492de
Rituraj0406 <riturajpiku01@gmail.com>
2025-10-06 22:40:59 +0530

    added initial files and connected the mongodb database

---
+++ models/product.js
@@ -0,0 +1,33 @@
+const getDb = require("../util/database").getDb;
+
+class Product {
+  constructor(title, price, description, imageUrl) {
+    this.title = title;
+    this.price = price;
+    this.description = description;
+    this.imageUrl = imageUrl;
+  }
+  save() {
+    const db = getDb();
+    return db.collection('products').insertOne(this)
+      .then(result => {
+        console.log('Product created:', result);
+      })
+      .catch(err => console.log(err));
+  }
+
+  static fetchAll(){
+    const db = getDb();
+    return db.collection('products')
+      .find()
+      .toArray()
+      .then(product => {
+        console.log('Fetched products:', product);
+        return product;
+      })
+      .catch(err => console.log(err));
+  }
+
+}
+
+module.exports = Product;
